0441b7a9e48ea29450c8d8531c8f31f22270bbd3
jgengle <jgengle@gmail.com>
2018-06-25 12:02:24 -0400

    Create SmartSheetTest.js

---
+++ SmartSheetTest.js
@@ -0,0 +1,14 @@
+
+var url = 'https://raw.githubusercontent.com/jgengle/ShalennialGroup/master/SmartSheetJson.json';
+var MyJas;
+
+
+    $.getJSON(url, function (data) {
+        //console.log(data);
+      //  alert(data.length);
+        alert('test');
+        //var seasons_ar = data.seasons;
+        //var seasonsul = array_to_ul(seasons_ar);
+        //$('#seasons').append(seasonsul);
+    
+});
